e267926c7e4a57fc0d5c838176a9d349f194cd82
Rebecca Moraes <rgabrielle@ciandt.com>
2022-07-26 12:04:51 -0300

    Criando teste para adição de ingrediente em estoque.

--- src/test/java/com/example/TDD/TddApplicationTests.java
+++ src/test/java/com/example/TDD/TddApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.TDD;
 
+import com.example.models.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
 class TddApplicationTests {
+	Armazem armazem = new Armazem();
+	@BeforeEach
+	void setUp() {
+		Armazem armazem = new Armazem();
+	}
 
 	@Test
-	void contextLoads() {
+	void cadastrarIngredienteEmEstoqueTest_NovoIngrediente() {
+		Ingrediente sorvete = new Base(TipoBase.Sorvete);
+
+		assertEquals(0, armazem.consultarQuantidadeDoIngredienteEmEstoque(sorvete));
+
+		armazem.cadastrarIngredienteEmEstoque(sorvete);
+		assertEquals(1, armazem.consultarQuantidadeDoIngredienteEmEstoque(sorvete));
+	}
+
+	@Test
+	void cadastraIngredienteEmEstoque_IngredienteJaCadastrado() {
+		Ingrediente sorvete = new Base(TipoBase.Sorvete);
+
+		armazem.cadastrarIngredienteEmEstoque(sorvete);
+
+		assertThrows(IllegalArgumentException.class, () -> armazem.cadastrarIngredienteEmEstoque(sorvete));
 	}
 
 }
