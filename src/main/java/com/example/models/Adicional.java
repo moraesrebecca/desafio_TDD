d835e1fb26770979198f36c0fccd4a19b43392ee
Rebecca Moraes <rgabrielle@ciandt.com>
2022-07-26 12:03:32 -0300

    Copiando interface adicional do projeto anterior.

---
+++ src/main/java/com/example/models/Adicional.java
@@ -0,0 +1,6 @@
+package com.example.models;
+
+public interface Adicional extends Ingrediente {
+
+}
+
--- src/main/java/com/example/models/Fruta.java
+++ src/main/java/com/example/models/Fruta.java
@@ -1,6 +1,6 @@
 package com.example.models;
 
-public class Fruta  implements Adicional,Comparable<Ingrediente>{
+public class Fruta  implements Adicional, Comparable<Ingrediente> {
      private TipoFruta tipoFruta;
 
      public Fruta(TipoFruta tipoFruta) {
